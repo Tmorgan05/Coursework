9769f6f3d247fb4b4ca9151e46fdc843977753a9
Tmorgan05 <TM2020.Amazon@gmail.com>
2023-04-14 21:17:05 -0500

    adding Block7, Grocery, Zoo Problems

---
+++ Block7/Block7.js
@@ -0,0 +1,35 @@
+
+//  block 7 - Instructions to brush your teeth.
+/*  1. Buy some tooth paste dentist approved.
+    2. Remove any protective covering from the base container holding the toothpaste.
+    3. Remove cap from toothpaste container.
+    4. Prepare toothbrush.  
+    5. Apply toothpaste on the bristles of the toothbrush.
+    6. Using the toothbrush with the toothpaste, litterally brush your teeth with back and forth motions.
+    7. After about two minutes of brushing all teeth, thoroughly rinse the inside of your mouth with water.
+    8. Wash toothbrush and replace in storage area.
+    9. Replace cap on toothpaste tube.  
+    10. Dry your mouth. */
+
+    let numberOfCows = 50;
+    const greet = "Hello World";
+    console.log('Hello World!')
+    // alert const greet a;
+    // //The number of umbrellas. let umb = 10 * 3;
+    // let aa= 5 + 1;
+    // alert(aa);
+    // // Answer.  const num = 5 + 1;
+    // //  Answer.  alert(num);
+    // /* I don't understand. I'm supposed to be in js-code.  Why would I add an external 
+    //    script file to an HTML file? */
+    // // Answer.  <script src="myscript.js"></script>
+    // Const= "Hello" + "World"
+    // // Answer.  Const str = "Hello" + " " + "World";
+    // let nme="name"
+    // const str= "Hello" + " " + "World." + " " + "I am" + nme;
+    // // Answer.  
+    // let str1 = "hello,";
+    // let str2 = "I am";
+    // let name = "Josh";
+    // alert (str1 + " "+ str2 + " " + name);
+
---
+++ Block7/Block71.js
@@ -0,0 +1,9 @@
+console.log('Combination to the Vault')
+/* I am going to code three calculations that provide the combination to the Vault. */
+let firstNum = 5 + 5;
+alert("Turn dial to the right stopping at " + firstNum);
+const secondNum = 50 - 10;
+alert("Turn dial to the left one full turn passing " + firstNum + " stopping at " + secondNum);
+let thirdNum = secondNum - 1;
+alert("Turn dial to the right stopping at " + thirdNum);
+alert("Congratulation! Enjoy.");
